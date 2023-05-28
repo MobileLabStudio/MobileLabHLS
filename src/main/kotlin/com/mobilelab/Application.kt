7add988a45dfe90f4180782ae1cb5dd579e02e18
Sebastian Bruliński <mobile.lab.contact@gmail.com>
2023-05-28 17:28:16 +0200

    Initial commit

---
+++ src/main/kotlin/com/mobilelab/Application.kt
@@ -0,0 +1,17 @@
+package com.mobilelab
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.mobilelab.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureHTTP()
+    configureSerialization()
+    configureRouting()
+}
---
+++ src/main/kotlin/com/mobilelab/plugins/HTTP.kt
@@ -0,0 +1,22 @@
+package com.mobilelab.plugins
+
+import io.ktor.server.plugins.partialcontent.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.application.*
+
+fun Application.configureHTTP() {
+    install(PartialContent) {
+            // Maximum number of ranges that will be accepted from a HTTP request.
+            // If the HTTP request specifies more ranges, they will all be merged into a single range.
+            maxRangeCount = 10
+        }
+    install(Compression) {
+        gzip {
+            priority = 1.0
+        }
+        deflate {
+            priority = 10.0
+            minimumSize(1024) // condition
+        }
+    }
+}
